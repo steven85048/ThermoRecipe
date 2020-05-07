2336a81a3ff7f9dce069c91adfacf3b0c1250696
Steven Tran <steven85048@gmail.com>
2020-05-07 14:23:07 -0700

    Add persistence into sqlite (and eventually postgresql); tested and working for a single recipe

---
+++ scraper/recipe_model.py
@@ -0,0 +1,59 @@
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import create_engine, ForeignKey, Column, Integer, String, DateTime
+from sqlalchemy.orm import relationship
+import datetime
+
+Base = declarative_base()
+
+class Recipe(Base):
+    __tablename__ = 'recipes'
+
+    id = Column(Integer, primary_key=True)
+
+    title = Column(String(250))
+    recipe_note = Column(String(1000))
+    url = Column(String(100))
+
+class Ingredients(Base):
+    __tablename__ = 'ingredients'
+
+    id = Column(Integer, primary_key=True)
+
+    recipe_id = Column(Integer, ForeignKey('recipes.id'), nullable=False)
+    recipe = relationship(Recipe, foreign_keys=[recipe_id])
+    
+    ingredient = Column(String(150), nullable=False)
+
+class Directions(Base):
+    __tablename__ = 'directions'
+
+    id = Column(Integer, primary_key=True)
+
+    recipe_id = Column(Integer, ForeignKey('recipes.id'), nullable=False)
+    recipe = relationship(Recipe, foreign_keys=[recipe_id])
+    
+    order = Column(Integer, nullable = False)
+    direction = Column(String(350), nullable = False)
+
+class Reviews(Base):
+    __tablename__ = 'reviews'
+
+    id = Column(Integer, primary_key=True)
+
+    recipe_id = Column(Integer, ForeignKey('recipes.id'), nullable=False)
+    recipe = relationship(Recipe, foreign_keys=[recipe_id])
+    
+    date = Column(DateTime, default=datetime.datetime.utcnow)
+    stars = Column(Integer)
+    helpful = Column(Integer)
+    description = Column(String(1000))
+
+def create_all(engine):
+    Base.metadata.create_all(engine)
+
+def drop_all(engine):
+    Base.metadata.drop_all(engine)
+
+if __name__ == '__main__':
+    engine = create_engine('sqlite:///./recipe-links-sqlite.db')
+    create_all(engine)
---
+++ scraper/recipe_service.py
@@ -0,0 +1,73 @@
+from datetime import datetime
+
+from scraper.recipe_model import Base, Ingredients, Recipe, Reviews, Directions, create_all, drop_all
+from scraper.scrape_recipe import RecipeScrape
+
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+import logging
+
+class RecipeService:
+    def __init__(self):
+        pass
+
+    def store_recipe(self, recipe_link, session):
+        self.scraper = RecipeScrape(recipe_link)
+        self.scraper.scrape()
+
+        recipe = self._store_recipe(session)
+        self._store_ingredients(session, recipe)
+        self._store_directions(session, recipe)
+        self._store_reviews(session, recipe)
+
+        session.commit()
+
+    def _store_recipe(self, session):
+        new_recipe = Recipe(title=self.scraper.title, recipe_note=self.scraper.description_text, url=self.scraper.recipe_link)
+        session.add(new_recipe)
+
+        # Need to flush session changes to get the ID of the recipe for foreign key purposes
+        session.flush()
+        return new_recipe
+
+    def _store_ingredients(self, session, recipe):
+        for ingredient in self.scraper.ingredients:
+            new_ingredient = Ingredients(recipe=recipe, 
+                                         ingredient=ingredient)
+            session.add(new_ingredient)
+
+    def _store_directions(self, session, recipe):
+        for index, direction in enumerate(self.scraper.directions):
+            new_direction = Directions( recipe=recipe, 
+                                        order=index, 
+                                        direction=direction)
+            session.add(new_direction)
+
+    def _store_reviews(self, session, recipe):
+        storeable_reviews = [self._to_storeable_review(r, recipe) for r in self.scraper.reviews]
+        for review in storeable_reviews:
+            session.add(review)
+
+    def _to_storeable_review(self, review, recipe):
+        new_review = Reviews(recipe=recipe, 
+                            date=datetime.strptime(review["date"], "%m/%d/%Y"), 
+                            stars=review['stars'], 
+                            description=review["description"], 
+                            helpful=review["helpful"])
+        return new_review
+
+if __name__ == '__main__':
+    logging.basicConfig()
+    logging.getLogger('sqlalchemy.engine').setLevel(logging.INFO)
+
+    engine = create_engine('sqlite:///./recipe-links-sqlite.db')
+    drop_all(engine)
+    create_all(engine)
+
+    Base.metadata.bind = engine
+    db_session = sessionmaker(bind=engine) 
+    session = db_session()
+
+    recipe_service = RecipeService()
+    recipe_service.store_recipe("https://www.allrecipes.com/recipe/228542/roasted-vegetables-with-spaghetti-squash/", session)
