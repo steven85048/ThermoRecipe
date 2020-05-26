769722c489f137fd3573afe2fbd5862264f0b208
Steven Tran <steven85048@gmail.com>
2020-05-26 11:52:06 -0700

    Redirect stdout in cron service to log

--- scraper/cron/schedule.py
+++ scraper/cron/schedule.py
@@ -1,8 +1,9 @@
 from crontab import CronTab
 
-cron = CronTab(user='ubuntu')
+user = 'ubuntu'
+cron = CronTab(user=user)
 
-job = cron.new(command='/usr/bin/flock -n /tmp/scraper.lockfile python3 ~/ThermoRecipe/scraper/cron/test.py')
+job = cron.new(command='/usr/bin/flock -n /tmp/scraper.lockfile python3 ~/ThermoRecipe/scraper/cron/test.py >> /home/{}/scraper.log 2>&1'.format(user))
 job.minute.every(1)
 
 cron.write()
