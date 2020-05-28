7e954fb9c33a158a003b67a6f81e707b14a1a74c
Steven Tran <steven85048@gmail.com>
2020-05-28 13:56:34 -0700

    Update cron one-off to update on reboot and take out process locking

--- scraper/cron/schedule.py
+++ scraper/cron/schedule.py
@@ -3,7 +3,7 @@ from crontab import CronTab
 user = 'ubuntu'
 cron = CronTab(user=user)
 
-job = cron.new(command='/usr/bin/flock -n /tmp/scraper.lockfile python3 ~/ThermoRecipe/scraper/cron/test.py >> /home/{}/scraper.log 2>&1'.format(user))
-job.minute.every(1)
+job = cron.new(command='cd /home/ubuntu/ThermoRecipe && python3 -m scraper.recipe_batch_processor >> /home/ubuntu/scraper.log')
+job.every_reboot()
 
 cron.write()
