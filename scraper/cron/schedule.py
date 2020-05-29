4cdbf9a895d5adc62712fbee7056ce3c7da42ba4
Steven Tran <steven85048@gmail.com>
2020-05-28 20:18:33 -0700

    Redirect standard error into standard out in crontab

--- scraper/cron/schedule.py
+++ scraper/cron/schedule.py
@@ -3,7 +3,7 @@ from crontab import CronTab
 user="ubuntu"
 cron = CronTab(user=user)
 
-job = cron.new(command='cd /home/ubuntu/ThermoRecipe && python3 -m scraper.scraper >> /home/ubuntu/scraper.log')
+job = cron.new(command='cd /home/ubuntu/ThermoRecipe && python3 -m scraper.scraper >> /home/ubuntu/scraper.log 2&>1')
 job.every_reboot()
 
 cron.write()
