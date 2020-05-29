2caaa3bf8972b64459b2f8397b9b809a75bba7e1
Steven Tran <steven85048@gmail.com>
2020-05-28 20:14:37 -0700

    Update crontab script to use new scraper script

--- scraper/cron/schedule.py
+++ scraper/cron/schedule.py
@@ -1,9 +1,8 @@
 from crontab import CronTab
 
-user = 'ubuntu'
 cron = CronTab(user=user)
 
-job = cron.new(command='cd /home/ubuntu/ThermoRecipe && python3 -m scraper.recipe_batch_processor >> /home/ubuntu/scraper.log')
+job = cron.new(command='cd /home/ubuntu/ThermoRecipe && python3 -m scraper.scraper >> /home/ubuntu/scraper.log')
 job.every_reboot()
 
 cron.write()
